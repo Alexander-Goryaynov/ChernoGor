0ad6a7212ba9f1322e2762491b3f4db4ef9f0bae
Flamenrage <55380311+Flamenrage@users.noreply.github.com>
2021-08-30 15:47:29 +0400

    Update TheNavbar.vue

--- resources/js/components/TheNavbar.vue
+++ resources/js/components/TheNavbar.vue
@@ -25,22 +25,48 @@
                     <div class="collapse navbar-collapse" id="navbarResponsive">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
-                                <router-link class="nav-link" :to="{name: 'home'}"> Главная </router-link>
+                                <router-link class="nav-link" :to="{name: 'home'}"> Главная</router-link>
                             </li>
                             <li class="nav-item">
                                 <router-link class="nav-link" :to="{name: 'categories'}">Наши услуги</router-link>
                             </li>
-                            <li class="nav-item">
+                            <li v-if="isAuthorized && !isAdmin" class="nav-item">
                                 <router-link class="nav-link" :to="{name: 'create-order'}">Создать заявку</router-link>
                             </li>
-                            <li class="nav-item">
-                                <router-link class="nav-link" :to="{name: 'register'}"> Регистрация </router-link>
+                            <li v-if="!isAuthorized" class="nav-item">
+                                <router-link class="nav-link" :to="{name: 'register'}"> Регистрация</router-link>
+                            </li>
+                            <li v-if="!isAuthorized" class="nav-item">
+                                <router-link class="nav-link" :to="{name: 'login'}"> Войти</router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link class="nav-link" :to="{name: 'login'}"> Войти </router-link>
+                                <router-link class="nav-link" :to="{name: 'notaries'}"> Нотариусы</router-link>
                             </li>
-                            <li class="nav-item">
-                                <router-link class="nav-link" :to="{name: 'notaries'}"> Нотариусы </router-link>
+                            <li v-if="isAuthorized && !isAdmin" class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#"
+                                             id="navbarDropdownMenuLink1" data-toggle="dropdown" aria-haspopup="true"
+                                             aria-expanded="false">
+                                    Личный кабинет
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink1">
+                                    <router-link class="dropdown-item" :to="{name: 'user-account'}">Аккаунт
+                                    </router-link>
+                                    <router-link class="dropdown-item" :to="{name: 'user-account-orders'}">Заказы
+                                    </router-link>
+                                </div>
+                            </li>
+                            <li v-if="isAuthorized && isAdmin" class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="http://example.com"
+                                   id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true"
+                                   aria-expanded="false">
+                                    Управление
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink2">
+                                    <router-link class="dropdown-item" :to="{name: 'admin-accounts'}">Аккаунты
+                                    </router-link>
+                                    <router-link class="dropdown-item" :to="{name: 'admin-orders'}">Заказы
+                                    </router-link>
+                                </div>
                             </li>
                         </ul>
                     </div>
@@ -52,10 +78,26 @@
 
 <script>
 export default {
-    name: "TheNavbar"
+    data() {
+        return {
+            isAdmin: true,
+            isAuthorized: true
+        }
+    }
 }
 </script>
 
 <style scoped>
+.dropdown:hover > .dropdown-menu {
+    display: block;
+}
 
+.dropdown-item.active, .dropdown-item:active {
+    background-color: white !important;
+    color: black !important;
+}
+
+.nav-link {
+    text-transform: none !important;
+}
 </style>
