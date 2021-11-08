850ef579fed06004fbc1bad59ca9e02dc9aefe8a
Flamenrage <55380311+Flamenrage@users.noreply.github.com>
2021-11-08 23:42:21 +0400

    Update MainContentComponent.vue

--- resources/js/components/MainComponentItems/MainContentComponent.vue
+++ resources/js/components/MainComponentItems/MainContentComponent.vue
@@ -10,7 +10,7 @@
                     </div>
                     <div class="col-md-4">
                         <router-link v-if="!isAuthorized" class="bord border-button" :to="{name: 'register'}">Зарегистрироваться</router-link>
-                        <router-link class="bord filled-button" :to="{name: 'create-order'}">Оставить заявку</router-link>
+                        <router-link v-else class="bord filled-button" :to="{name: 'create-order'}">Оставить заявку</router-link>
                     </div>
                 </div>
             </div>
